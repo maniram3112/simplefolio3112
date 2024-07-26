93740069c380bce53f2a6aae4f3d22e183c2dda7
unknown <maniramthota12@gmail.com>
2024-07-26 11:43:43 +0530

    resolved few minor issues

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/simplefolio3112/',
   plugins: [
     vue(),
   ],
+  base: '/simplefolio3112',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
